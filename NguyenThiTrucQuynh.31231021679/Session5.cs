5b55e266a0ab5b022c90e53bd14aafb23b901ddd
Nguyễn Thị Trúc Quỳnh <quynhnguyen.31231021679@st.ueh.edu.vn>
2024-11-08 13:17:15 +0700

    Update Session5.cs

--- NguyenThiTrucQuynh.31231021679/Session5.cs
+++ NguyenThiTrucQuynh.31231021679/Session5.cs
@@ -85,37 +85,167 @@ namespace NguyenThiTrucQuynh._31231021679
                
             } while (true);
         }
-        /// <summary>
-        /// Write a C# function that takes a number as a parameter and checks whether the number is prime or not.
-        /// </summary>
-        public static void Question_03()
+        public static void Question2()
         {
-
+            static float calculatingFactorial(int n)
+            {
+                int demSo = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    demSo *= i;
+                }
+                return demSo;
+            }
+            Console.Write("Enter n: ");
+            int n = int.Parse(Console.ReadLine());
+            float factorial = calculatingFactorial(n);
+            Console.WriteLine("the factorial of number {0}: {1}", n, factorial);
 
         }
-        /// <summary>
-        /// Write a C# function to print: all prime numbers that less than a number (enter prompt keyboard).
 
-        /// </summary>
-        public static void Question_04a()
+        public static void Question3()
         {
+            static int checkingPrimeNumber(int n)
+            {
+                int demUoc = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    if (n % i == 0)
+                        demUoc++;
+                }
+                return demUoc;
+            }
+            Console.Write("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+            int demUoc = checkingPrimeNumber(n);
+            if (demUoc == 2)
+                Console.WriteLine($"{n} is a prime number");
+            else
+                Console.WriteLine($"{n} is not a prime number");
 
+        }
+        public static void Question4_1()
+        {
+            Console.Write("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Prime numbers less than {n}:");
+            for (int i = 2; i < n; i++)
+            {
+                if (IsPrime(i) == 1)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        static int IsPrime(int number)
+        {
+            int value;
+            int demUoc = 0;
+            for (int j = 1; j <= number; j++)
+            {
+                if (number % j == 0)
+                    demUoc++;
+            }
+            if (demUoc == 2)
+            {
+                value = 1;
+            }
+            else value = 0;
+            return value;
+        }
+
+        static void Question4_2()
+        {
+            static void NPrimeNumber(int N)
+            {
+                int c = 0;
+                for (int i = 2; c < N; i++)
+                {
+                    if (IsPrime(i) == 1)
+                    {
+                        Console.Write(i + " ");
+                        c++;
+                    }
+                }
+            }
+            Console.Write("Enter a number: ");
+            int N = int.Parse(Console.ReadLine());
+            NPrimeNumber(N);
+        }
+
+        static void Question5()
+        {
+            static int checkingPerfectNumber(int n)
+            {
+                int value = 0;
+                int tong = 0;
+                for (int i = 1; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        tong += i;
+                    }
+                }
+                if (tong == n)
+                    value = 1;
+                else value = 0;
+                return value;
+            }
+            static void printingPerfectNumber()
+            {
+                for (int i = 1; i < 1000; i++)
+                {
+                    if (checkingPerfectNumber(i) == 1)
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+            }
+            Console.Write("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+            int value = checkingPerfectNumber(n);
+            if (value == 1)
+            {
+                Console.WriteLine($"{n} is a perfect number");
+            }
+            else
+                Console.WriteLine($"{n} is not a perfect number");
+            Console.WriteLine("all perfect number that less than 1000: ");
+            printingPerfectNumber();
+            Console.WriteLine();
         }
       
-
-        /// <summary>
-        ///  Write a C# function to check whether a number is "Perfect" or not. Then print all perfect number that less than 1000.
-        /// </summary>
-        public static void Question_05()
+        static void Question6()
         {
-
-        }
-        /// <summary>
-        /// Write a C# function to check whether a string is a pangram or not.
-        /// </summary>
-        public static void Question_06()
-        {
-
+            static bool checkPangram(string str)
+            {
+                bool[] alphabet = new bool[26];
+                int index = 0;
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if ('A' <= str[i] && str[i] <= 'Z')
+                        index = str[i] - 'A';
+                    else if ('a' <= str[i] && str[i] <= 'z')
+                        index = str[i] - 'a';
+                    else
+                        continue;
+                    alphabet[index] = true;
+                }
+                for (int i = 0; i <= 25; i++)
+                {
+                    if (alphabet[i] == false)
+                        return false;
+                }
+                return true;
+            }
+            Console.Write("Enter a sentence: ");
+            string str = Console.ReadLine();
+            if (checkPangram(str) == true)
+                Console.WriteLine("{0} is a pangram", str);
+            else
+                Console.WriteLine("{0} is not a pangram", str);
         }
     }
 }
