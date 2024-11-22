4d106aa11d4de0cb412c33724a19fecc100e1af6
Administrator <Administrator@N301PC26>
2024-11-22 16:18:31 +0700

    session8

--- NguyenThiTrucQuynh.31231021679/Session8.cs
+++ NguyenThiTrucQuynh.31231021679/Session8.cs
@@ -11,13 +11,23 @@ namespace NguyenThiTrucQuynh._31231021679
         public static void Main(string[] args)
         {
             string s1 = "con cho quynh nhu!";
+            string s2 = "con cho bao nghi";
             //to input a string and print it.
             Console.WriteLine(s1);
+            Console.WriteLine(s2);
             Console.WriteLine(len(s1));
+
+
             print(s1);
             Console.WriteLine();
             printReverse(s1);
             Console.WriteLine($"Tong cac tu cua chuoi la: {CountWords(s1)}");
+
+
+            compare(s1 , s2);
+            Console.WriteLine(compare(s1, s2));
+
+
 
             //to separate individual characters from a string.
             static void print(string s)
@@ -72,9 +82,23 @@ namespace NguyenThiTrucQuynh._31231021679
                 return wordCount;
             }
 
-
+       
+            static int compare(string s1, string s2)
+            {
+                int min = s1.Length > s2.Length ? s1.Length : s2.Length;
+                int i = 0;  
+                while (i < min)
+                {
+                    if ((s1[i] > s2[i])) return 1;
+                    else if ((s1[i] < s2[i])) return -1;
+                    i++;
+                }
+                    if (s1.Length > s2.Length) return 1;
+                    else if (s1.Length < s2.Length) return -1;
+                    return 0;
+            }
             
-
+           
 
         }
     }
